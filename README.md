0af3ff53aa6b1d689f99b9d52cb1e0ce0ff6a7df
강민수 <naman79@naver.com>
2023-11-06 10:45:03 +0900

    second

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # mashidda_vue02
+
+npm install -g @vue/cli
+
+vue create mashidda_vue02
+
+cd mashidda_vue02
+
+npm add router
+
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>MASHIDDA</h1>
+    <div>
+        <button>프로필</button>
+        <button>검색</button>
+        <button>설정</button>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
